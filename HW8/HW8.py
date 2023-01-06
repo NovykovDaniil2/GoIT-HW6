262b952a40ef784e8d157230fef5f3f9f99df576
NovykovDaniil2 <relax2934@gmail.com>
2023-01-06 23:39:09 +0200

    Task Edits

--- HW8/HW8.py
+++ HW8/HW8.py
@@ -1,14 +1,17 @@
 import datetime
 
-#Test dictionary
+#Test list
 users=[
-    {'name':'Mark' , 'birthday': datetime.datetime(year=2010 , month=1, day=3)},
-    {'name':'Alex' , 'birthday': datetime.datetime(year=1999 , month=9 , day=1)},
-    {'name':'Kate' , 'birthday': datetime.datetime(year=2001 , month=4 , day=26)},
-    {'name':'Sue'  , 'birthday': datetime.datetime(year=2006 , month=10 , day=30) }
-]
+    {'name': 'Derek Thomas', 'birthday': datetime.datetime(2023, 1, 8, 0, 0)}, 
+    {'name': 'Julie Mathis', 'birthday': datetime.datetime(2023, 1, 9, 0, 0)},
+    {'name': 'Jacqueline Griffin', 'birthday': datetime.datetime(2023, 1, 10, 0, 0)},
+    {'name': 'Kenneth Smith', 'birthday': datetime.datetime(2023, 1, 11, 0, 0)},
+    {'name': 'Ivan Robertson', 'birthday': datetime.datetime(2023, 1, 12, 0, 0)},
+    {'name': 'John Woods', 'birthday': datetime.datetime(2023, 1, 30, 0, 0)},       #The date does not match the task condition
+    {'name': 'Laura Christensen', 'birthday': datetime.datetime(2023, 1, 5, 0, 0)}  #The date does not match the task condition
+    ]
 
-def get_birthdays_per_week(users : list) -> None: 
+def get_birthdays_per_week(users) -> None: 
 
     result={
     'Monday': [],
@@ -17,22 +20,26 @@ def get_birthdays_per_week(users : list) -> None:
     'Thursday': [],
     'Friday': []
     }
+    
+    today = datetime.date.today()
 
     for i in users:
-        #The current day of the week converted to the name of the day of the week in English
-        current_day = i.get('birthday').strftime('%A')
-        
-        #Checking the days of the week
-        if current_day == 'Monday' or current_day == 'Saturday' or current_day == 'Sunday':
-            result.get('Monday').append(i.get('name'))
-        elif current_day == 'Tuesday' :
-            result.get('Tuesday').append(i.get('name'))
-        elif current_day == 'Wednesday' :
-            result.get('Wednesday').append(i.get('name'))
-        elif current_day == 'Thursday' :
-            result.get('Thursday').append(i.get('name'))
-        elif current_day == 'Friday' :
-            result.get('Friday').append(i.get('name'))
+        if i.get('birthday').year == today.year and i.get('birthday').month == today.month and today.day <= i.get('birthday').day <= (today + datetime.timedelta(days=7)).day :
+            
+            #The current day of the week converted to the name of the day of the week in English
+            current_day = i.get('birthday').strftime('%A')
+            
+            #Checking the days of the week
+            if current_day == 'Monday' or current_day == 'Saturday' or current_day == 'Sunday':
+                result.get('Monday').append(i.get('name'))
+            elif current_day == 'Tuesday' :
+                result.get('Tuesday').append(i.get('name'))
+            elif current_day == 'Wednesday' :
+                result.get('Wednesday').append(i.get('name'))
+            elif current_day == 'Thursday' :
+                result.get('Thursday').append(i.get('name'))
+            elif current_day == 'Friday' :
+                result.get('Friday').append(i.get('name'))
 
 
     for day_and_name in result.items():
@@ -41,6 +48,11 @@ def get_birthdays_per_week(users : list) -> None:
         res_str = f"{day_and_name[0]}:"
         for name in day_and_name[1]:
             res_str+=f' {name},'
-        print(res_str[:-1])
+        print(res_str[:-1]+'\n')
 
 print(get_birthdays_per_week(users))
+
+#Output: Monday: Derek Thomas, Julie Mathis
+         #Tuesday: Jacqueline Griffin
+         #Wednesday: Kenneth Smith
+         #Thursday: Ivan Robertson
