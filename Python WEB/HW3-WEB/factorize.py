856d8e72bc64fbc079f6deb7ac409f5911f2752a
NovykovDaniil <121320203+NovykovDaniil@users.noreply.github.com>
2023-04-07 23:48:42 +0300

    bugfix

--- Python WEB/HW3-WEB/factorize.py
+++ Python WEB/HW3-WEB/factorize.py
@@ -18,33 +18,37 @@ def factorize(*number) -> list:
     return all_divisors
 
 
-def factorize_process(num: int) -> list:
-    divisors=[]
-    i=1
-    while i * i <= num:
-        if not num % i:
-            divisors += [i]
-            k=num // i
-            if k != i:
-                divisors += [k]
-        i+=1
-    divisors.sort()
-    return divisors
-
-def start_factorize_process(*number):
-    result = []
-    with Pool(processes=cpu_count()) as pool:
-        result+=pool.map(factorize_process, (num for num in number))
+def factorize_process(*nums) -> list:
+    result = {}
+    for num in nums:
+        divisors=[]
+        i=1
+        while i * i <= num:
+            if not num % i:
+                divisors += [i]
+                k=num // i
+                if k != i:
+                    divisors += [k]
+            i+=1
+        divisors.sort()
+        result[num] = divisors
     return result
 
 
 if __name__ == '__main__':
+    
     time_point_1 = perf_counter()
     factorize(*range(100000))
     time_point_2 = perf_counter()
-    start_factorize_process(*range(100000))
+    
+    pool = Pool(cpu_count())
+    result = pool.apply_async(factorize_process, range(100000))
     time_point_3 = perf_counter()
-    print('\033[1mResult for 10.000 elements:\033[0m')
-    print(f'\033[31mSynchronous implementation:\033[0m {time_point_2 - time_point_1}s')
-    print(f'\033[32mMultiprocessing implementation:\033[0m {time_point_3 - time_point_2}s')
-    print(f'\033[1mMultiprocessing is faster than synchronous implementation on: {round((time_point_3 - time_point_2) / (time_point_2 - time_point_1) * 100)}%\033[0m')
+    
+    time_result_sync = time_point_2 - time_point_1
+    time_result_async = time_point_3 - time_point_2
+
+    print('\033[1mResult for 100.000 elements:\033[0m')
+    print(f'\033[31mSynchronous implementation:\033[0m {time_result_sync}s')
+    print(f'\033[32mMultiprocessing implementation:\033[0m {time_result_async}s')
+    print(f'\033[1mMultiprocessing is faster than synchronous implementation on: {100 - round((time_result_async * 100) / time_result_sync)}%\033[0m')
