42eb8dd829ac553079bf57103a854b198e37388f
NovykovDaniil2 <relax2934@gmail.com>
2023-04-12 23:18:55 +0300

    Update exchange_rate.py

--- Python WEB/HW5-WEB/exchange_rate.py
+++ Python WEB/HW5-WEB/exchange_rate.py
@@ -6,7 +6,7 @@ from datetime import datetime, timedelta
 from aiohttp import ClientSession, ClientConnectionError
 from prettytable import PrettyTable
 
-CURRENCIES = ["USD", "EUR"]
+
 SUCCESSFUL_STATUS = 200
 
 
@@ -31,20 +31,20 @@ async def request(url: str) -> dict:
             logging.error(f"Connection error: {url}", str(err))
 
 
-async def format_data(url: str) -> tuple:
+async def format_data(url: str, currencies: list) -> tuple:
     responce = await request(url)
     data = {
         rate["currency"]: {"buy": rate["saleRateNB"], "sell": rate["purchaseRateNB"]}
         for rate in responce["exchangeRate"]
-        if rate["currency"] in sorted(CURRENCIES)
+        if rate["currency"] in sorted(currencies)
     }
     date = responce["date"]
     return (data, date)
 
 
-async def create_columns() -> list:
+async def create_columns(currencies: list) -> list:
     table_columns = ["Date"]
-    for currance in sorted(CURRENCIES):
+    for currance in sorted(currencies):
         columns_for_currance = [currance + " Buy", currance + " Sell"]
         table_columns.extend(columns_for_currance)
     return table_columns
@@ -63,8 +63,8 @@ async def get_rate_data(exchange_data: tuple) -> list:
     return rate_data
 
 
-async def pretty_view(exchange_data: tuple) -> PrettyTable:
-    table_columns = await create_columns()
+async def pretty_view(exchange_data: tuple, currencies: list) -> PrettyTable:
+    table_columns = await create_columns(currencies)
     rate_table = PrettyTable(table_columns)
     rate_data = await get_rate_data(exchange_data)
     for day_date_date in rate_data:
@@ -74,17 +74,18 @@ async def pretty_view(exchange_data: tuple) -> PrettyTable:
 
 async def main() -> PrettyTable:
     url_pattern = "https://api.privatbank.ua/p24api/exchange_rates?date="
-
     try:
         days_range = int(sys.argv[1]) if len(sys.argv) > 1 else 1
         if days_range > 10:
             raise ValueError
+        currencies = [currency.upper() for currency in sys.argv[2:]]
+        if not currencies:
+            return "You did not specify the currency or days range"
+        urls = await format_url(url_pattern, days_range)
+        data = [await format_data(url, currencies) for url in urls]
+        return await pretty_view(data, currencies)
     except ValueError:
-        return "The range of days must be an integer and not exceed the value of 10"
-    
-    urls = await format_url(url_pattern, days_range)
-    data = [await format_data(url) for url in urls]
-    return await pretty_view(data)
+        return "You did not specify the days range or entered the currency code incorrectly. The correct format is three letters of the Latin alphabet."
 
 
 if __name__ == "__main__":
