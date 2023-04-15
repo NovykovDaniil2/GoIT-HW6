094cc1f52abaf1aa2bc3d584d7317b975cc143e4
NovykovDaniil <121320203+NovykovDaniil@users.noreply.github.com>
2023-04-15 17:15:21 +0300

    Update factorize.py

--- Python WEB/HW3-WEB/factorize.py
+++ Python WEB/HW3-WEB/factorize.py
@@ -1,6 +1,7 @@
 from multiprocessing import Pool, cpu_count
 from time import perf_counter
 
+
 def factorize(*number) -> list:
     all_divisors = []
     for num in number:
@@ -38,16 +39,17 @@ def factorize_process(*nums) -> list:
 if __name__ == '__main__':
     
     time_point_1 = perf_counter()
-    factorize(*range(100000))
+    result_sync = factorize(*range(100000))
     time_point_2 = perf_counter()
     
-    pool = Pool(cpu_count())
-    result = pool.apply_async(factorize_process, range(100000))
+    with Pool(processes=cpu_count()) as pool:
+        result_async = pool.map(factorize_process, range(100000))
     time_point_3 = perf_counter()
     
     time_result_sync = time_point_2 - time_point_1
     time_result_async = time_point_3 - time_point_2
 
+    
     print('\033[1mResult for 100.000 elements:\033[0m')
     print(f'\033[31mSynchronous implementation:\033[0m {time_result_sync}s')
     print(f'\033[32mMultiprocessing implementation:\033[0m {time_result_async}s')
